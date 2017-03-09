2eb601025049af59874baf1a7bbf8af542a1a957
Arrielle Kooiman <arrielle.kooiman@gmail.com>
2017-03-09 10:39:55 -0600

    adding functions to factory

--- public/scripts/controllers/task.controller.js
+++ public/scripts/controllers/task.controller.js
@@ -1,46 +1,23 @@
 myApp.controller('TaskController', ['$http', 'TaskFactory', function($http, TaskFactory){ //first task factory is a DEPENDENCY and relates to task.factory.js second one is being passed in as a 'random' perameter
   console.log('The task controller was created');
   var self = this;
-  self.newTask = {};
+  // self.newTask = {};
   self.taskList = [];
   self.someThingToGoOnTheView = TaskFactory.testProperty;
   self.someRandomArray = TaskFactory.testArray;
   self.taskList = TaskFactory.allTasks;
 
+  self.addTask = function(){
+    TaskFactory.addTask(self.newTask);
+  }
 
+  self.deleteTask = function(taskId){
+    TaskFactory.deleteTask(taskId);
+  }
 
-  self.addTask = function addTask() { //if I want to call it in the index, I need to add it to self
-    $http({
-      method: 'POST',
-      url: '/tasks',
-      data: self.newTask
-    }).then(function(response){
-      TaskFactory.updateTasks();
-      self.newTask = {};
-      console.log('was clicked');
-    });//ends.then
-  }//ends addTask function
-
-self.deleteTask = function(taskId){
-  console.log('delete button was clicked');
-  $http({
-    method: 'DELETE',
-    url: '/tasks/' + taskId
-  }).then(function(response){
-    TaskFactory.updateTasks();
-  });
-}//ends deleteTask function
-
-
-self.completeTask = function(taskId){
-  console.log('complete button was clicked');
-  $http({
-    method: 'PUT',
-    url: '/tasks/' + taskId
-  }).then(function(response){
-    TaskFactory.updateTasks();
-  });
-}//ends deleteTask function
+  self.completeTask = function(taskId){
+    TaskFactory.completeTask(taskId);
+  }
 //
 // self.completeTask = function(taskId){
 //   $http({
--- public/scripts/factories/task.factory.js
+++ public/scripts/factories/task.factory.js
@@ -1,7 +1,11 @@
 myApp.factory('TaskFactory', ['$http', function($http){
 
+var self = this;
 var testArrayVar = ['queso', 'bagel', 'donald bagel', 'cheeeeese', 'bread', 'caroline'];
 testArrayVar.pop();
+
+self.newTask = {};
+
 
 var factoryTasks = { list:[] }; //object with property list with key of an empty array that will be replaced by the data that we get back from the ajax request.
 getTasks();
@@ -16,11 +20,46 @@ function getTasks() {
   });//ends .then
 } //ends getTasks function
 
+function addTask() { //if I want to call it in the index, I need to add it to self
+  $http({
+    method: 'POST',
+    url: '/tasks',
+    data: self.newTask
+  }).then(function(response){
+    getTasks();
+    self.newTask = {};
+    console.log('was clicked');
+  });//ends.then
+}//ends addTask function
+
+function completeTask(taskId){
+  console.log('complete button was clicked');
+  $http({
+    method: 'PUT',
+    url: '/tasks/' + taskId
+  }).then(function(response){
+    getTasks();
+  });
+}//ends completeTask function
+
+function deleteTask(taskId){
+  console.log('delete button was clicked');
+  $http({
+    method: 'DELETE',
+    url: '/tasks/' + taskId
+  }).then(function(response){
+    getTasks();
+  });
+}//ends deleteTask function
+
   return {
     testProperty: 'taco',
     testArray: testArrayVar,
     allTasks: factoryTasks,
-    updateTasks: getTasks
+    updateTasks: getTasks,
+    completeTasks: completeTask,
+    deleteTasks: deleteTask,
+    addTasks: addTask
   };//return is like module.exports. Stuff inside gets passed back.
 }]);//return is how other things access this information made public to everything in the application
 
